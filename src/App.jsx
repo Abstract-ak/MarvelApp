967800026307f7487aac4f697d185b0c0c6f8849
Abstract-ak <ash.king0205@gmail.com>
2025-02-26 17:44:15 +0530

    Hero component developed for banner image

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import CompanyOverview from "./components/CompanyOverview";
+import ProductRange from "./components/ProductRange";
+import Partners from "./components/Partners";
+import CallToAction from "./components/CallToAction";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-grow">
-        <Banner />
+        {/* <Banner /> */}
+        <Hero />
         {/* Add other sections */}
+        <CompanyOverview />
+        <ProductRange />
+        <Partners />
+        <CallToAction />
       </main>
       <Footer />
     </div>
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -78,8 +78,10 @@ const Banner = () => {
           </div>
         </Swiper>
       </div>
-      <CompanyOverview />
+      {/* <CompanyOverview />
       <ProductRange />
+      <Partners />
+      <CallToAction /> */}
     </>
   );
 };
